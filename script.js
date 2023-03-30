37b09322c4ee2aff790aef9fb24f2e7757147080
sushantkdm <109025450+sushantkdm@users.noreply.github.com>
2023-03-30 20:04:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+
+let age= document.querySelector("#age")
+let name=document.querySelector("#name")
+let btn=document.querySelector("#btn")
+
+let p= new Promise(function(resolve, reject){
+    setInterval(function(){
+        let age1=age.ariaValueMax;
+        let name1=name.value;
+
+        if(age1>18){
+            alert(`Welcome,${name1}. You can vote`)
+        }
+        else{
+            alert(`Oh sorry ${name1}. You aren't old enough`)
+        }
+
+    },4000)
+})
